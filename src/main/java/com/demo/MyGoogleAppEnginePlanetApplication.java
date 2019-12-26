4c3c0592be7d0a7002bdfb0b552cb9f05366bd01
ravi1 <ravi.bh.java@gmail.com>
2019-12-26 12:01:23 +0530

    changed from new branch

--- src/main/java/com/demo/MyGoogleAppEnginePlanetApplication.java
+++ src/main/java/com/demo/MyGoogleAppEnginePlanetApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyGoogleAppEnginePlanetApplication{
+public class MyGoogleAppEnginePlanetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyGoogleAppEnginePlanetApplication.class, args);
